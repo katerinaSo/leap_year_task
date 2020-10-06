831e48f96ba88bff37a08e57a8792d03913c4c67
Katerina Solstad (katya sorok) <katya.solstad@gmail.com>
2020-10-06 17:17:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+'''The year must be evenly divisible by 4;
+   If the year can also be evenly divided by 100, it is not a leap year;
+   unless...
+   The year is also evenly divisible by 400. Then it is a leap year.'''
+   
+'''1904 1908 1912 1916 2004 1928 1932 1976 2020 2000 1988'''
